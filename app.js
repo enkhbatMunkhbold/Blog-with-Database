c85335e81a38cda428a9ba804be3944710ef8904
unknown <e_munkhbold@yahoo.com>
2021-10-04 21:52:07 -0700

    Post the posts in the homepage

--- app.js
+++ app.js
@@ -13,9 +13,14 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const postSchema = {
   title: String,
@@ -24,54 +29,59 @@ const postSchema = {
 
 const Post = mongoose.model("Post", postSchema);
 
-let posts = [];
-
-app.get("/", function(req, res){
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
-    });
+app.get("/", function(req, res) {
+  Post.find({}, (err, posts) => {
+    if(!err) {
+      res.render("home", {
+        startingContent: homeStartingContent,
+        posts: posts
+      });
+    }
+  });
 });
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
+app.get("/about", function(req, res) {
+  res.render("about", {
+    aboutContent: aboutContent
+  });
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+    contactContent: contactContent
+  });
 });
 
-app.get("/compose", function(req, res){
+app.get("/compose", function(req, res) {
   res.render("compose");
 });
 
-app.post("/compose", function(req, res){
-  const post = new Post ({
+app.post("/compose", function(req, res) {
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
   });
 
-  // posts.push(post);
-  post.save();
-
-  res.redirect("/");
+  post.save(err => {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
 
 });
 
-app.get("/posts/:postName", function(req, res){
-  const requestedTitle = _.lowerCase(req.params.postName);
-
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
-
-    if (storedTitle === requestedTitle) {
+app.get("/posts/:postId", function(req, res) {
+  const requestedPostId = req.params.postId;
+  Post.findOne({
+    _id: requestedPostId
+  }, (err, post) => {
+    if (!err) {
       res.render("post", {
         title: post.title,
         content: post.content
       });
     }
   });
-
 });
 
 app.listen(3000, function() {
